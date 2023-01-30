de04bd0df41e72c1983f72b1d01075955b433407
TehcBing <99237853+TehBing@users.noreply.github.com>
2023-01-30 22:57:27 +0800

    Update reward Page
    Update reward Page

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'reward-page',
+        loadChildren: () => import('../reward-page/reward-page.module').then(m => m.RewardPagePageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
