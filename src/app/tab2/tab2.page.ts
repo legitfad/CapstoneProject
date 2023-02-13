445a097ecd9933fe6f841bec955a07c8a53b4cb9
Fadzil Selamat <fadzilakmal@gmail.com>
2023-02-13 14:09:27 +0800

    code

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController, ModalController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 import { AdvertUI, DataService } from '../services/data.service';
 
 @Component({
@@ -11,14 +12,14 @@ import { AdvertUI, DataService } from '../services/data.service';
 export class Tab2Page {
 
   adverts: AdvertUI [] = [];
-  authService: any;
 
   constructor(
     private data: DataService, 
     private cd: ChangeDetectorRef,
     private alerts: AlertController, 
     private modal: ModalController, 
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { 
       this.data.getAds().subscribe(res => {console.log(res);
       this.adverts = res;
@@ -33,6 +34,6 @@ export class Tab2Page {
   logout() {
     this.authService.logout();
   }
-
+ 
 
 }
